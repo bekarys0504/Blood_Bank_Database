54aff7a08a25320d6584fcb127ab50ec01706daf
Bekarys Gabdrakhimov <bekarys0504@gmail.com>
2021-04-08 11:45:40 +0200

    corrected datediff to 42 days

--- View.sql
+++ View.sql
@@ -3,6 +3,6 @@ use BloodBank;
 drop view BloodAvailable;
 
 create view BloodAvailable as select BloodType, sum(amount) from Donation natural join Donor
-where DATEDIFF(CURDATE(), DonationDate) AND MedicalCheck='Pass' AND BeenUsed='Unused' group by BloodType;
+where DATEDIFF(CURDATE(), DonationDate) > 42 AND MedicalCheck='Pass' AND BeenUsed=False group by BloodType;
 
 select * from BloodAvailable;
